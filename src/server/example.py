6c8c47c74c2c73c94379b19856785c4dcff76c37
Philipp Hempel <phempel@programmfabrik.de>
2018-03-05 17:38:59 +0100

    added example for process callback and performing database queries

--- src/server/example.py
+++ src/server/example.py
@@ -3,7 +3,15 @@
 import os
 import json
 import yaml
-import datetime
+
+from datetime import datetime, date
+import calendar
+import locale
+from dateutil.relativedelta import relativedelta
+
+from time import sleep
+from threading import Thread
+
 from context import EasydbException
 from context import InvalidValueError
 from context import get_json_value
@@ -29,9 +37,13 @@ def easydb_server_start(easydb_context):
 	# method name that is called: 'pre_update'
 	easydb_context.register_callback('db_pre_update', {'callback': 'pre_update'})
 
-	# register callback that are called after data was exported / transported
+	# register a callback that is called after data was exported
 	# export objects as YML
 	easydb_context.register_callback('export_produce', {'callback': 'export_as_yml'})
+
+	# register a process callback
+	# check objects for expiration date and send mails
+	easydb_context.register_callback('process', {'name': 'check_expiration_date'})
 
 
 # helper method to generate a unique id for an object
@@ -40,11 +52,8 @@ def generate_unique_id(easydb_context):
 	return easydb_context.next_unique_id_prefixed('medium_nummer', 16, 'medium_')
 
 
-# helper method that creates a elasticsearch request for the name of a medienart object
-def search_for_medienart(easydb_context, name):
-	logger = easydb_context.get_logger('example-plugin.search_for_medienart')
-	logger.info("Search for medienart '%s'" % name)
-
+# helper method to perform a search using an EasydbContext
+def perform_search_easydb(easydb_context, query, logger = None):
 	# get the user id from the current session
 	user_id = None
 	try:
@@ -54,26 +63,45 @@ def search_for_medienart(easydb_context, name):
 			logger.error("Could not get user id from session")
 			return None
 	except Exception as e:
-		print e
+		logger.error("Could not get user id from session: %s" % e)
 		return None
+	search_result = easydb_context.search("user", user_id, search_query)
+	if logger is not None:
+		logger.debug("Search Result: %s" % json.dumps(search_result, indent = 4))
+
+	return search_result
+
+
+# helper method to perform a search using an EasydbProcessContext
+def perform_search_process(easydb_context, connection, session_identifier, query, logger = None):
+	search_result = easydb_context.search(connection, "user", session_identifier, query)
+	if logger is not None:
+		logger.debug("Search Result: %s" % json.dumps(search_result, indent = 4))
+
+	return search_result
+
+
+# helper method that creates a elasticsearch request for the name of a medienart object
+def search_for_medienart(easydb_context, name):
+	logger = easydb_context.get_logger('example-plugin.search_for_medienart')
+	logger.info("Search for medienart '%s'" % name)
 
 	# define the search query
 	search_query = {
 		"type": "object",
 		"generate_rights": False,
-		"include_fields": ["medienart._id"],     # the field that we want to get as a search result
+		"include_fields": ["medienart._id"], # the field that we want to get as a search result
 		"search": [{
-			"type":   "in",                      # search on of the values in 'in'
-			"bool":   "should",
-			"fields": ["medienart.name"],        # the name of the field that we search for
-			"in":     [name]                     # list of values that the field should have (only on in this case)
+			"type": "in", # search on of the values in 'in'
+			"bool": "should",
+			"fields": ["medienart.name"], # the name of the field that we search for
+			"in": [name] # list of values that the field should have (only on in this case)
 		}]
 	}
 	logger.debug("Search Request: %s" % json.dumps(search_query, indent = 4))
 
 	# perform the search and return the result
-	search_result = easydb_context.search("user", user_id, search_query)
-	logger.debug("Search Result: %s" % json.dumps(search_result, indent = 4))
+	search_result = perform_search_easydb(easydb_context, search_query, logger)
 
 	return search_result
 
@@ -179,9 +207,14 @@ def export_as_yml(easydb_context, parameters):
 	exporter = easydb_context.get_exporter()
 
 	# check if the export definition fits to this plugin
-	export_definition = get_json_value(exporter.getExport(), "export.produce_options.plugin", False)
-	if str(export_definition) != "example_plugin":
+	produce_options = get_json_value(exporter.getExport(), "export.produce_options", False)
+	if str(get_json_value(produce_options, "plugin", False)) != "example_export":
 		return
+
+	# check if the produce option for exporting the YML with or without tags is a boolean, else set it to false
+	with_tags = get_json_value(produce_options, "with_tags", False)
+	if not isinstance(with_tags, bool):
+		with_tags = False
 
 	# load exported files (need to be exported as JSON files)
 	export_dir = exporter.getFilesPath()
@@ -205,6 +238,7 @@ def export_as_yml(easydb_context, parameters):
 			logger.debug("Converting JSON file %s to YML" % file_path)
 
 			try:
+				# load and parse the json file
 				file = open(file_path, "r")
 				content = json.loads(file.read().decode('utf-8'))
 				file.close()
@@ -217,9 +251,17 @@ def export_as_yml(easydb_context, parameters):
 					tmp_filename = os.path.abspath("%s/../tmp/objects.yml" % export_dir)
 
 					with open(tmp_filename, "w") as yml_file:
-						yaml.safe_dump({
+						# define the final dict that will be converted to YML
+						object_output = {
 							"objects": objects
-						}, yml_file, default_flow_style = False)
+						}
+
+						# depending on the produce options, export the YML with or without tags
+						if with_tags:
+							yaml.dump(object_output, yml_file, default_flow_style = False)
+						else:
+							yaml.safe_dump(object_output, yml_file, default_flow_style = False)
+
 						yml_file.close()
 
 						logger.debug("Saved objects as %s" % tmp_filename)
@@ -234,6 +276,158 @@ def export_as_yml(easydb_context, parameters):
 					logger.debug("Found no 'objects' array")
 			except Exception as e:
 				logger.warn("Could not convert JSON to YML: %s" % str(e))
+
+
+# run method to start threads for process plugins
+# method is called at server start
+def run(easydb_context):
+	logger = easydb_context.get_logger('example-plugin.process')
+	logger.info("run")
+
+	# set up a thread that runs once a day and checks if the expiration
+	t = Thread(target=check_expiration_date, args=(easydb_context,))
+	t.start()
+	t.join()
+
+
+# repeatedly check the expiration date of objects
+# easydb_context: EasydbProcessContext
+def check_expiration_date(easydb_context):
+
+	logger = easydb_context.get_logger('example-plugin.check_expiration_date')
+
+	# connect to the database
+	connection = easydb_context.db_connect('check_expiration_date')
+
+	# load the configuration
+	config = easydb_context.get_config(connection)
+
+	# search all objects of the type 'medium', using a SQL query, where the expiration data is in less then a week
+	while True:
+
+		# create and format a date that is 7 days in the future
+		days_in_future = 7
+		date = datetime.now() + relativedelta(days = days_in_future)
+		date_str = date.strftime("%Y-%m-%d")
+
+		# get a database cursor
+		cursor = connection.cursor()
+
+		# build the Postgres statement
+		sql = """
+			SELECT m."id:pkey", m.titel, m.identifier, m.ablaufdatum, m.":owner:ez_user:id",
+				u.login, u.name, u.firstname, u.displayname, u.frontend_language,
+				e.address
+			FROM medium m JOIN ez_user u ON m.":owner:ez_user:id" = u."ez_user:id"
+			AND ablaufdatum <= '%s'
+			JOIN "ez_user:email" e ON e."ez_user:id" = u."ez_user:id"
+			AND e.is_primary AND e.use_for_email AND e.send_email AND address IS NOT NULL
+		"""
+
+		# perform the request and save the result
+		connection.cursor().execute(sql % date_str)
+		result = connection.cursor().fetchall()
+
+		mails_to_send = {}
+
+		logger.debug("%s results found" % len(result))
+		for row in result:
+			try:
+				# information about the object
+				identifier = row["identifier"]
+				titel = row["titel"]
+				ablaufdatum = datetime.strptime(row["ablaufdatum"], "%Y-%m-%d")
+
+				# mail address
+				address = row["address"] if len(row["address"]) else None
+
+				if address is None:
+					continue
+
+				# user information
+				user_displayname = row["displayname"]
+				if user_displayname is None or len(user_displayname) < 1:
+					user_displayname = ""
+					user_displayname += row["firstname"] if row["firstname"] is not None else ""
+					user_displayname += " " if len(user_displayname) > 0 else ""
+					user_displayname += row["name"] if row["name"] is not None else ""
+
+					if len(user_displayname) < 1:
+						user_displayname = row["login"] if row["login"] is not None else ""
+
+						if len(user_displayname) < 1:
+							user_displayname = address
+
+				# set the locale according to the user language
+				user_lang = row["frontend_language"]
+
+				# write the text for the mail in german or english
+				# TODO get the l10n translations from the server
+				mail_text = None
+
+				if user_lang == "de-DE":
+					locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
+					mail_text = "Medium %s (%s) laeuft am %s ab" % (
+						identifier,
+						titel,
+						"%s, den %s" % (
+							calendar.day_name[ablaufdatum.weekday()],
+							ablaufdatum.strftime("%d.%m.%Y")
+						)
+					)
+				else:
+					locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+					mail_text = "Medium %s (%s) expires %s" % (
+						identifier,
+						titel,
+						"%s, %s" % (
+							calendar.day_name[ablaufdatum.weekday()],
+							ablaufdatum.strftime("%Y-%m-%d")
+						)
+					)
+
+				logger.info(mail_text)
+
+				if not address in mails_to_send:
+					mails_to_send[address] = {
+						"language": user_lang,
+						"name": user_displayname,
+						"mail_text": []
+					}
+
+				mails_to_send[address]["mail_text"].append(mail_text)
+
+			except Exception as e:
+				raise e
+
+		for adr in mails_to_send:
+
+			mail = None
+			if mails_to_send[adr]["language"] == "de-DE":
+				mail = "Hallo %s,\n\ndie folgenden Objekte laufen innerhalb der nächsten %d Tage ab:\n\n%s\n\nMit freundlichen Gruessen"
+			else:
+				mail = "Hello %s,\n\nthe following objects expire during the next %d days:\n\n%s\n\nRegards"
+
+			logger.debug("Mail to %s:\n%s" % (
+				adr, mail % (
+					mails_to_send[adr]["name"],
+					days_in_future,
+					"\n - ".join(mails_to_send[adr]["mail_text"])
+				)
+			))
+
+			# TODO send the mail instead of logging the mail text
+
+		# sleep for one hour
+		sleep(60 * 60)
+
+		# TODO dont send more then one mail for each object
+
+
+# method to cleanup process plugin resources before the server stops
+def stop(easydb_context):
+	logger = easydb_context.get_logger('example-plugin.process')
+	logger.info("stop")
 
 
 # method that is called when API Endpoint <server>/api/plugin/base/example-plugin/config is called
